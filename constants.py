2e47dd4e9a404a29e3fc606a7281e8a9c2917753
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2025-06-17 18:30:19 -0700

    Add presets for different fire shapes

--- constants.py
+++ constants.py
@@ -2,5 +2,18 @@
 FIRE_WIDTH = 1440
 FIRE_HEIGHT = 960
 MAX_INTENSITY = 255
-DECAY_MULT=11
-RAND_MULT=10
+
+###Presets
+#normal
+DECAY_MULT=5
+RAND_MULT=4
+
+# #floating sparks
+# DECAY_MULT=11
+# RAND_MULT=10
+
+
+# #big fire
+# DECAY_MULT=5
+# RAND_MULT=5
+
