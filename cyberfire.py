ec80046aa6ba4126ad499a01639a09d3d15720ba
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2025-06-19 12:53:58 -0700

    Split out modes into their own file

--- cyberfire.py
+++ cyberfire.py
@@ -16,56 +16,10 @@ from core import (FIRE_HEIGHT, FIRE_WIDTH, clear_fixed_pixels, do_fire,
                   initialize_palette_electric, initialize_palette_fire,
                   initialize_palette_gray, initialize_palette_sunset,
                   initialize_palette_toxic, render_tool_radius, update_image)
+from modes import FireLineMode, FireMode, FireRectMode, FixMode, ModeType
 from tools import (FireBrushTool, FireEraseTool, FireLineTool, FireRectTool,
                    FixBrushTool, FixEraseTool, HighlightFixedTool, Tool,
                    ToolType)
-
-
-class ModeType(Enum):
-    FIRE = auto()
-    FIX = auto()
-    FIRE_LINE = auto()
-    FIRE_RECT = auto()
-
-
-class Mode:
-    def __init__(self, lmb_tool_type, rmb_tool_type):
-        self.lmb_tool_type = lmb_tool_type
-        self.rmb_tool_type = rmb_tool_type
-
-    def activate(self, tools):
-        # Deactivate all tools except highlight
-        for ttype, tool in tools.items():
-            if ttype != ToolType.HIGHLIGHT_FIXED:
-                tool.trigger_off()
-        return tools
-
-    def deactivate(self, tools):
-        # Deactivate all tools except highlight
-        for ttype, tool in tools.items():
-            if ttype != ToolType.HIGHLIGHT_FIXED:
-                tool.trigger_off()
-        return tools
-
-
-class FireMode(Mode):
-    def __init__(self):
-        super().__init__(ToolType.FIRE_BRUSH, ToolType.FIRE_ERASE)
-
-
-class FixMode(Mode):
-    def __init__(self):
-        super().__init__(ToolType.FIX_BRUSH, ToolType.FIX_ERASE)
-
-
-class FireLineMode(Mode):
-    def __init__(self):
-        super().__init__(ToolType.FIRE_LINE, ToolType.FIRE_LINE)
-
-
-class FireRectMode(Mode):
-    def __init__(self):
-        super().__init__(ToolType.FIRE_RECT, ToolType.FIRE_RECT)
 
 
 class FireWindow(QMainWindow):
---
+++ modes.py
@@ -0,0 +1,50 @@
+from enum import Enum, auto
+
+from tools import ToolType
+
+
+class ModeType(Enum):
+    FIRE = auto()
+    FIX = auto()
+    FIRE_LINE = auto()
+    FIRE_RECT = auto()
+
+
+class Mode:
+    def __init__(self, lmb_tool_type, rmb_tool_type):
+        self.lmb_tool_type = lmb_tool_type
+        self.rmb_tool_type = rmb_tool_type
+
+    def activate(self, tools):
+        # Deactivate all tools except highlight
+        for ttype, tool in tools.items():
+            if ttype != ToolType.HIGHLIGHT_FIXED:
+                tool.trigger_off()
+        return tools
+
+    def deactivate(self, tools):
+        # Deactivate all tools except highlight
+        for ttype, tool in tools.items():
+            if ttype != ToolType.HIGHLIGHT_FIXED:
+                tool.trigger_off()
+        return tools
+
+
+class FireMode(Mode):
+    def __init__(self):
+        super().__init__(ToolType.FIRE_BRUSH, ToolType.FIRE_ERASE)
+
+
+class FixMode(Mode):
+    def __init__(self):
+        super().__init__(ToolType.FIX_BRUSH, ToolType.FIX_ERASE)
+
+
+class FireLineMode(Mode):
+    def __init__(self):
+        super().__init__(ToolType.FIRE_LINE, ToolType.FIRE_LINE)
+
+
+class FireRectMode(Mode):
+    def __init__(self):
+        super().__init__(ToolType.FIRE_RECT, ToolType.FIRE_RECT)
